4443410e3faa94b8d5f9780606d95f9f117634d7
USER <USER@LAPTOP-FMJRBMUO>
2025-09-08 11:47:28 +0700

    ini praktikum 1

---
+++ prahalpro_2025_B_2511531018/src/pekan1/HelloWorld.java
@@ -0,0 +1,19 @@
+package pekan1;
+
+public class HelloWorld  {
+	public static void main(String[] args) {
+        //program pertama
+       /*komentar
+         * dua baris atau lebih
+         * pertemuan 1 praktkum alpro
+         */
+	     System.out.println("Hello World");
+	     System.out.println();
+	     System.out.println("Program ini menghasilkan");
+	     System.out.println("Tujuh baris output");
+	     System.out.println("/ \\ // \\\\ /// \\\\");
+	     System.out.println("This"+" program prints a\n"
+	            + "quote from the Gettysburg Address.");
+	    
+}
+}
