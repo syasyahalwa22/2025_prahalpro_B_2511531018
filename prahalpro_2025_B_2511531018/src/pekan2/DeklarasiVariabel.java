30ef435d8cd9b5cc44643f0d063e033b674bec11
USER <USER@LAPTOP-FMJRBMUO>
2025-09-15 11:47:47 +0700

    Alpro pekan 2

---
+++ prahalpro_2025_B_2511531018/src/pekan2/DeklarasiVariabel.java
@@ -0,0 +1,22 @@
+package pekan2;
+
+public class DeklarasiVariabel {
+	/* program java
+	*latihan
+	*tentang pendeklarasian variabel
+	*/
+   static int umur=25; /* variabel dapat langsung diinisiasi */
+   public static void main(String[] args) {
+	   int kode;
+	   boolean isDibawahUmur; /* perhatikan penulisan nama Variabel */
+	   kode = 1234; /* pengisisan variabel (assignment)*/
+	   double gaji; /* deklarasi variabel dapat dimana saja */
+	   gaji = 5500000.23;
+	   isDibawahUmur = true;
+	   System.out.println("Status: "+isDibawahUmur);
+	   System.out.println("kode:"+kode);
+	   System.out.println("Umur:"+umur);
+	   System.out.println("Gaji:"+gaji); 
+
+  }
+}
