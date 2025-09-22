3b116e47e5e8b19f2cab80ab3719e4f2001b3f30
syasyahalwa22 <syasyahalwa22@gmail.com>
2025-09-22 15:48:59 +0700

    Add files via upload

---
+++ prahalpro_2025_B_2511531018/src/pekan2/DataMobil.java
@@ -0,0 +1,20 @@
+package pekan2;
+
+public class DataMobil {
+	public static void main(String[] args) {
+		// Data mobil
+		int stok = 3;
+		float harga = 250_000_000f;
+		char kategori = 'S'; 
+		boolean tersedia = true;
+		
+		// Output
+		System.out.println("=== Data Mobil ===");
+		System.out.println();
+		System.out.println("Stok     : " + stok);
+		System.out.println("Harga    : " + harga);
+		System.out.println("Kategori : " + kategori);
+		System.out.println("Tersedia : " + tersedia);
+
+	}
+}
